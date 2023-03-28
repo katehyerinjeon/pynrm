0bd4aad152ebee220d93776824309ceb59e6bb67
Kate Jeon <katehyerinjeon@gmail.com>
2023-03-27 20:10:00 -0400

    Add tests for Pedigree class

--- pynrm/tests/test_pedigree.py
+++ pynrm/tests/test_pedigree.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import unittest
 from pynrm.Pedigree import Pedigree
 
@@ -8,3 +9,14 @@ class TestSimulator(unittest.TestCase):
 
     def tearDown(self):
         self.pedigree = None
+
+    def test_pedigree(self):
+        with self.assertRaises(TypeError):
+            Pedigree("wrong data type")
+
+        self.assertEqual(self.pedigree.data.shape[1], 5, "expected 5 columns")
+        self.assertEqual(self.pedigree.data[self.pedigree.data["sex"] == "M"].shape[0], 500, "expected 500 males")
+        self.assertEqual(self.pedigree.data[self.pedigree.data["sex"] == "F"].shape[0], 500, "expected 500 females")
+        self.assertEqual(self.pedigree.data["gen"][0], 0, "expected generation 0")
+        self.assertTrue(pd.isna(self.pedigree.data["sire"][0]), "expected sire NA")
+        self.assertTrue(pd.isna(self.pedigree.data["dam"][0]), "expected dam NA")
